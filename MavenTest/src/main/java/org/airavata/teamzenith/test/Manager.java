79db3202e99bd42cffda49cfb551659d707ac5e3
Atul Mohan <atulmohan.mec@gmail.com>
2016-01-27 13:18:59 -0500

    Added OutputStream to test

--- MavenTest/src/main/java/org/airavata/teamzenith/test/Manager.java
+++ MavenTest/src/main/java/org/airavata/teamzenith/test/Manager.java
@@ -2,6 +2,7 @@ package org.airavata.teamzenith.test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Properties;
@@ -13,8 +14,10 @@ public class Manager {
 	}
 
 	public boolean transferFile(String fileName) throws IOException{
-			FileInputStream pwr = new FileInputStream(getClass().getResource("/"+fileName).getFile());
-	        System.out.println(pwr.read());
+			FileOutputStream pwr = new FileOutputStream(getClass().getResource("/"+fileName).getFile());
+			pwr.write("Sample data".getBytes());
+			FileInputStream fis = new FileInputStream(getClass().getResource("/"+fileName).getFile());
+	        System.out.println(fis.read());
 			return true;
 		}
 		
