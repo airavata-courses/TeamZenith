f7d82a01cfe0373ee0daef590652e78e3bd5dcaf
arpiagariu <arpiagariu@gmail.com>
2016-02-12 22:14:23 -0500

    Removed correct this from the SH file

--- Project_One_Submission/configpbs.sh
+++ Project_One_Submission/configpbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #PBS -l nodes=1:ppn=16 
-#correct this #PBS -l walltime=00:10:00 
+#PBS -l walltime=00:10:00 
 # no need to specify a queue on Karst, except if you need the debug or interactive queue 
 #load the mpi module 
 #cd to working directory 
