5495df1b5816e9e7c8590c3f244f7b2dad83ad67
arpiagariu <arpiagariu@gmail.com>
2016-02-24 14:10:09 -0500

    Submit using Angular
    Submit Form to Rest services with response on same page

---
+++ EndPoint/target/classes/static/app.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+
+var helloApp = angular.module("SG", [ 'ngResource' ]);
+helloApp.controller("HttpController", [ '$scope', '$resource',
+		function($scope, $resource) {
+			$scope.users = [];
+			
+			$scope.saveUser = function(){
+				//alert($scope.name);
+				$scope.users.push({ 'name':$scope.name, 'username': $scope.username, 'size':$scope.size, 'email':$scope.email });		
+				// Create a resource class object
+				//
+				var User = $resource('/upload');
+				// Call action method (save) on the class 
+				//
+				User.save({name:$scope.name,username:$scope.username,size:$scope.size,email:$scope.email}, function(response){
+					$scope.datapost = response;
+					console.log(response);
+				});
+				
+				$scope.firstname='';
+				$scope.username='';
+				$scope.size='';
+				$scope.email='';
+			}
+			
+		} ]);
+		
