edc68056c88ec952fda593b5c3b7d6626664835a
Anuj Bhandar <bhandar.anuj@gmail.com>
2016-04-22 03:38:05 -0400

    Update install.sh

--- GatewayApp/install.sh
+++ GatewayApp/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mvn clean;
-mvn install;
-mv ./target/GatewayApp-0.1.0.war /usr/share/tomcat7/webapps/gateway.war
+
+mvn -f /home/ec2-user/builds/GatewayApp/pom.xml clean;
+mvn -f /home/ec2-user/builds/GatewayApp/pom.xml install;
+mv /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war /usr/share/tomcat7/webapps/gateway.war
 sudo service tomcat7 restart
 
 
