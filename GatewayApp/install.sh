fbb6e0c7dc7d8edefc9adc0544c28b7f4a1280b8
Anuj Bhandar <anujbhan@iu.edu>
2016-04-23 16:50:59 -0400

    issue fixes from milestone-4

--- GatewayApp/install.sh
+++ GatewayApp/install.sh
@@ -2,7 +2,7 @@
 
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml clean;
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml install;
-mv /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war /usr/share/tomcat7/webapps/gateway.war
+cp /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war /usr/share/tomcat7/webapps/gateway.war
 sudo service tomcat7 restart
 
 
