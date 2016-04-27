f6f8f6612db2d06eb2f514d3d238481f357285ad
Anuj Bhandar <anujbhan@iu.edu>
2016-04-27 01:43:55 -0400

    changes to install and appspec

--- GatewayApp/install.sh
+++ GatewayApp/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#kill $(ps aux | grep 'sudo -u ec2-user java -jar'| awk '{print $2}');
+ps aux | grep 'sudo -u ec2-user java -jar'| awk '{print $2}' | xargs kill -9
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml clean;
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml install;
 chmod a+w /home/ec2-user/builds/GatewayApp/target
 sudo -u ec2-user java -jar /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war &
-#cp /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war /usr/share/tomcat7/webapps/gateway.war
-#sudo service tomcat7 restart
+
 
 
