2eeac2fbd6cf1da649bc79441a366eb57b7f55f0
Anuj Bhandar <bhandar.anuj@gmail.com>
2016-04-27 01:17:16 -0400

    Update install.sh

--- GatewayApp/install.sh
+++ GatewayApp/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kill $(ps aux | grep 'sudo -u ec2-user java -jar'| awk '{print $2}');
+#kill $(ps aux | grep 'sudo -u ec2-user java -jar'| awk '{print $2}');
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml clean;
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml install;
 chmod a+w /home/ec2-user/builds/GatewayApp/target
