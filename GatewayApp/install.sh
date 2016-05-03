a075e19b99d8f261d007143e8c27057d755bcd7c
Anuj Bhandar <bhandar.anuj@gmail.com>
2016-05-03 02:20:34 -0400

    update to install script to include server logs

--- GatewayApp/install.sh
+++ GatewayApp/install.sh
@@ -4,7 +4,8 @@ ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml clean;
 mvn -f /home/ec2-user/builds/GatewayApp/pom.xml install;
 chmod a+w /home/ec2-user/builds/GatewayApp/target
-sudo -u ec2-user java -jar /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war &
+sudo -u ec2-user java -jar /home/ec2-user/builds/GatewayApp/target/GatewayApp-0.1.0.war >> /home/ec2-user/GatewayServer.log 2>&1 &
+sleep 60
 
 
 
