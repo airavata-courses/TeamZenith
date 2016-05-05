615ba3432536089645e11cb82c726ead8f80cddf
Anuj Bhandar <bhandar.anuj@gmail.com>
2016-05-05 16:42:22 -0400

    minor property file change

--- GatewayAPI/install.sh
+++ GatewayAPI/install.sh
@@ -3,8 +3,8 @@
 ps aux | grep 'java -jar'| awk '{print $2}' | xargs kill -9
 mvn -f /home/ec2-user/builds/GatewayAPI/pom.xml clean;
 mvn -f /home/ec2-user/builds/GatewayAPI/pom.xml install;
-chmod a+w /home/ec2-user/builds/GatewayAPI;
-chmod a+w /home/ec2-user/builds/GatewayAPI/*;
+chmod a+w /home/ec2-user/builds/GatewayAPI
+chmod a+w /home/ec2-user/builds/GatewayAPI/*
 sudo -u ec2-user java -jar /home/ec2-user/builds/GatewayAPI/target/GatewayAPI-0.1.0.war >> /home/ec2-user/GatewayServer.log 2>&1 &
 sleep 60
 
